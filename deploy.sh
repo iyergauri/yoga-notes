f36d9555fb97ee100aedb0c1ca898ab25e3ff5e8
Gauri Iyer <iyergauri>
2021-02-20 14:18:08 -0800

    wish there was a script for bimboification

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,16 @@
 #!/bin/sh
 printf "%s\n" "commit the markdown to master first!"
-git checkout deploy 
 cd ..
 mkdir temp
 mv yoga-notes/* temp 
 mv temp/site/* yoga-notes
 cd yoga-notes
 git add .
+git checkout deploy 
 git commit -m "deploy site"
 git push origin deploy
+cd ..
+rm -rf temp
+cd yoga-notes
 git checkout master
 git pull origin master
